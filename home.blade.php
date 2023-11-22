9511536fe9f50b3ce8b4a6f42e0ff5d60dccee05
Muhammad Raffa Fadellah <122192352+Yuxsha@users.noreply.github.com>
2023-11-22 07:27:04 +0700

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,19 @@
+@extends("template")
+
+@section("title")
+Home
+@endsection
+
+@section("content")
+
+<img src="{{ url('media/pemandangan_laut.jpg') }}" class="img-fluid">
+
+<h1>
+    Halo bla bla bla
+</h1>
+
+@endsection
+
+@section("title1")
+Home
+@endsection
